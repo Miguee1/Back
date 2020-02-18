61940c3100e43020d462908b448bfda49ac112cf
migue <miguenw@gmail.com>
2020-02-17 21:29:24 -0600

    init

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+//settings
+
+//middlewares
+
+//Routers
+
+//statics files
+
+//empezando el servidor
+app.listen(3000, () => {
+    console.log('server on port 3000');
+
+});
